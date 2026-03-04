64d4fd16b862b8a463614219cff054107bfef6c9
jcwright16 <jasoncraigwright@gmail.com>
2026-03-03 23:56:41 -0500

    Big 'ol update

--- scripts/roll.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-echo -n "Enter your character's race: " 
-read character_race
-echo -n "Enter your character's class: "
-read character_class
-echo -n "Enter target filename: "
-read filename
-
-python roll_character.py $character_race $character_class $filename
